8580b1c926c52a7f42da770c77ac644ebbe1c93b
daisy1754 <d-part@hotmail.co.jp>
2016-07-03 15:36:16 -0700

    Remove badlogic.png image

--- core/src/jp/gr/java_conf/daisy/tetris/Tetris.java
+++ core/src/jp/gr/java_conf/daisy/tetris/Tetris.java
@@ -3,17 +3,14 @@ package jp.gr.java_conf.daisy.tetris;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Tetris extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
@@ -21,13 +18,11 @@ public class Tetris extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
