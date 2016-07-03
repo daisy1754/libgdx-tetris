09d5e72c20fa8553fa046482de744a17bf9b344e
daisy1754 <d-part@hotmail.co.jp>
2016-07-03 15:59:38 -0700

    Make background color dark-blue

--- core/src/jp/gr/java_conf/daisy/tetris/Tetris.java
+++ core/src/jp/gr/java_conf/daisy/tetris/Tetris.java
@@ -19,7 +19,7 @@ public class Tetris extends ApplicationAdapter {
 
   @Override
   public void render() {
-    Gdx.gl.glClearColor(1, 0, 0, 1);
+    Gdx.gl.glClearColor(0, 0, 0.2f, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
     camera.update();
